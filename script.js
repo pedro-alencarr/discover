83eac3ea331ffc98d36077213ac05560cabce80c
pedro <contatopedroalencarr@gmail.com>
2024-02-08 17:34:40 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+const html = document.documentElement
+
+/*
+
+if(html.classlist.contains('light')){
+     html.classlist.remove('light')    
+} else {
+  html.classlist.add('light')
+} 
+
+*/
+
+ html.classList.toggle('light')
+
+}
